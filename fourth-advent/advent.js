9a5ec1f927afd18f81c9291078c0db7fff2006f7
Ferran Petit <fpmartiwork@gmail.com>
2023-02-05 16:06:19 +0100

    Start fourth advent

--- fourth-advent/advent.js
+++ fourth-advent/advent.js
@@ -1,16 +1,26 @@
+function fitsInOneBox(boxes) {
+  boxes.forEach((box) => {
+    const boxMeasurements = Object.values(box);
+    const conditional = box.find((measure) => Object.values(measure));
+    console.log(boxMeasurements);
+    console.log(measurements);
+  });
+  return false;
+}
+
 fitsInOneBox([
   { l: 1, w: 1, h: 1 },
   { l: 2, w: 2, h: 2 },
 ]); // true
 
-const boxes = [
+fitsInOneBox([
   { l: 1, w: 1, h: 1 },
   { l: 2, w: 2, h: 2 },
   { l: 3, w: 1, h: 3 },
-];
+]); // false
 
-function fitsInOneBox(boxes) {
-  return false;
-}
-
-fitsInOneBox(boxes); // false
+fitsInOneBox([
+  { l: 1, w: 1, h: 1 },
+  { l: 3, w: 3, h: 3 },
+  { l: 2, w: 2, h: 2 },
+]); // true
